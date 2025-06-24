c419b1771125d3361f5c9721575a9100d40d3099
Malena2021 <malenarojas2021@gmail.com>
2025-06-23 20:17:49 -0400

    iniciado mi proyecto de django

---
+++ api/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Agente, Usuario,Propiedad
+
+admin.site.register(Agente)
+admin.site.register(Usuario)
+admin.site.register(Propiedad)
+
+# Register your models here.
+
+
